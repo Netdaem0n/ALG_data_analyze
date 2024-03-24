7d0d641bc7a8fd0bcfe30282fe5c3ef784eb4660
Netdaemon <bakalets@gmail.com>
2024-03-24 16:56:54 +1100

    v4

--- notebooks/case.py
+++ notebooks/case.py
@@ -1,1 +1,16 @@
 #место для твоего кода
+def extract_datetime(data):
+    print(len(data))
+    data = data.split(', ')
+    test_data = data[1].split(' ')
+    test = int(test_data[1][:2])
+
+    if test >= 18:
+        return 'Вечер'
+    elif test >= 12:
+        return 'День'
+    else:
+        return 'Утро'
+
+print(extract_datetime('Thu Jul 30, 2020 21:25 UTC'))
+Tue Feb 05, 2019
